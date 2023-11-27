734c36b1f1683fb3f33c6ec8e61978e1e4482344
= <=>
2023-11-27 14:28:26 -0300

    projetopronto

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const inputTarefa = document.getElementById('tarefa');
     const btnAdicionar = document.getElementById('Addtarefa');
     const tarefasPendentes = document.getElementById('tarefaspendentes');
+    const tarefasFinalizadas = document.getElementById('tarefasfinalizadas');
 
     btnAdicionar.addEventListener('click', function() {
         adicionarTarefa();
@@ -19,9 +20,25 @@ document.addEventListener('DOMContentLoaded', function() {
         if (tarefaTexto !== '') {
             const li = document.createElement('li');
             li.textContent = tarefaTexto;
+
+            const botao = document.createElement('button');
+            botao.textContent = 'Finalizar';
+            botao.addEventListener('click', function() {
+                finalizarTarefa(li);
+            });
+
+            li.appendChild(botao);
+
             tarefasPendentes.appendChild(li);
 
             inputTarefa.value = '';
         }
+    }
+
+    function finalizarTarefa(tarefa) {
+        tarefasPendentes.removeChild(tarefa);
+        tarefasFinalizadas.appendChild(tarefa);
+        const botaoFinalizar = tarefa.querySelector('button');
+        botaoFinalizar.parentNode.removeChild(botaoFinalizar);
     }
 });
